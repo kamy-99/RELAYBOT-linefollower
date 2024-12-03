6cf4588285c3f61bc5c986f756c6d09da37007a2
iLootYou <leon.kremer@student.nhlstenden.com>
2024-12-03 15:42:04 +0100

    Fully updated for this week, needs work for line following and PID

--- src/main.cpp
+++ src/main.cpp
@@ -1,274 +1,54 @@
-// #include <Arduino.h>
-
-// #define trig 4
-// #define echo 7
-// #define MotorA1 8 // MotorA1 at port 8 digital
-// #define MotorA2 10 // MotorA2 at port 10 analog
-// #define MotorB1 11 // MotorB1 at port 11 analog
-// #define MotorB2 9 // MotorB2 at port 9 analog
- 
-// void forward();
-// void backward();
-// void stop();
-// void spin_Right();
-// void spin_Left();
-// void turn_Right();
-// void turn_Left();
-// void sonar();
-// void sonarCheck();
-// void forwardCheck();
-// void backwardCheck();
-// void stopCheck();
-// void spin_RightCheck();
-// void spin_LeftCheck();
-// void turn_RightCheck();
-// void turn_LeftCheck();
-
-
-// int pwm;
-// int amount = 0;
- 
-// void setup()
-// {
-//  Serial.begin(9600);
-//  pinMode(MotorA1, OUTPUT); // MotorA1 is output
-//  pinMode(MotorA2, OUTPUT); // MotorA2 is output
-//  pinMode(MotorB1, OUTPUT); // MotorB1 is output
-//  pinMode(MotorB2, OUTPUT); // MotorB2 is output
-//  pinMode(trig, OUTPUT);
-//  pinMode(echo, INPUT);
-// }
- 
-// void loop()
-// {
-//   // delay(1000);
-//   // forward();
-//   // stop();
-//   // backward();
- 
-// // 1st
-//   // turn_Left();
-//   // spin_Right();
-//   // turn_Right();
-//   // spin_Left();
-//   // stop();
- 
-//   sonar();
-// }
-
-// void sonarCheck()
-// {
-//   static unsigned long timer;
-//   if (millis() > timer)
-//   {
-//     //sonar();
-//     timer = millis() + 250;
-//   }
-// }
-
-// void forwardCheck()
-// {
-//   static unsigned long timer;
-//   if (millis() > timer)
-//   {
-//     //forward();
-//     timer = millis() + 200;
-//   }
-// }
-
-// void backwardCheck()
-// {
-//   static unsigned long timer;
-//   if (millis() > timer)
-//   {
-//     //backward();
-//     timer = millis() + 200;
-//   }
-// }
- 
-// void stopCheck()
-// {
-//   static unsigned long timer;
-//   static bool state;
-//   if (millis() > timer)
-//   {
-//     //stop();
-//     timer = millis() + 1000;
-//   }
-// }
- 
-// void spin_RightCheck()
-// {
-//   static unsigned long timer;
-//   if (millis() > timer)
-//   {
-//     //spin_Right();
-//     timer = millis() + 510;
-//   }
-// }
-
-// void spin_LeftCheck()
-// {
-//   static unsigned long timer;
-//   if (millis() > timer)
-//   {
-//     //spin_Left();
-//     timer = millis() + 550;
-//   }
-// }
-
-// void turn_RightCheck()
-// {
-//   static unsigned long timer;
-//   if (millis() > timer)
-//   {
-//     //turn_Right();
-//     timer = millis() + 2625;
-//   }
-// }
-
-// void turn_LeftCheck()
-// {
-//   static unsigned long timer;
-//   if (millis() > timer)
-//   {
-//     //turn_Left();
-//     timer = millis() + 1800;
-//   }
-// }
- 
- 
-// void sonar() // printing distance
-// {
-//   digitalWrite(trig, LOW);
-//   delayMicroseconds(2);
-//   digitalWrite(trig, HIGH);
-//   delayMicroseconds(10);
-//   digitalWrite(trig, LOW);
- 
-//   long duration = pulseIn(echo, HIGH);
-//   int distance = (duration * 0.017); // Distance in cm
- 
-//   Serial.print("Distance: ");
-//   Serial.print(distance);
-//   Serial.println(" cm");
- 
-//   if (distance <= 40)
-//   {
-//     forward();
-//     if (distance <= 35)
-//     {
-//       forward();
-//       if (distance <= 30)
-//       {
-//         spin_Left();
-//         turn_Right();
-//         turn_Right();
-//         spin_Left();
-//         stop();
-//       }
-//     }
-//   }
-//   else
-//   {
-//     forward();
-//   }
-//   return;
-// }
- 
-// void forward()
-// {
-//   digitalWrite(MotorA1, 0);
-//   analogWrite(MotorB2, 0);
-//   analogWrite(MotorA2, 250); // Left motor runs at full speed
- 
-//   for(int pwm = 255; pwm < 256; pwm++)
-//   {
-//     analogWrite(MotorB1, pwm - 12); // Right motor with PWM control, slightly reduced
-//     //delay(200);
-//     forwardCheck();
-//   }
-//   // delay(5000);
-// }
- 
-// void backward()
-// {
-//   analogWrite(MotorA2, 0);
-//   analogWrite(MotorB1, 0);
-//   analogWrite(MotorA1, 250); // Left motor runs at full speed
- 
-//   for(int pwm = 255; pwm < 256; pwm++)
-//   {
-//     analogWrite(MotorB2, pwm -17); // Right motor with PWM control, slightly reduced
-//     backwardCheck();
-//   }
-//   //delay(5000);
-// }
- 
-// void stop() // stop all motors
-// {
-//   analogWrite(MotorA1, 0);
-//   analogWrite(MotorA2, 0);
-//   analogWrite(MotorB1, 0);
-//   analogWrite(MotorB2, 0);
-//   stopCheck();
-// }
- 
-// void spin_Right() // spin right on its axle in a 90 degree angle
-// {
-//   analogWrite(MotorA1, 0); // left backward
-//   analogWrite(MotorA2, 255); // left forward full speed
-//   analogWrite(MotorB1, 0); // right forward
-//   analogWrite(MotorB2, 255); // right backward full speed
-//   //delay(510); // 455
-//   spin_RightCheck();
-// }
- 
-// void spin_Left() // spin left on its axle in a 90 degree angle
-// {
-//   analogWrite(MotorA1, 250); // left backward full speed
-//   analogWrite(MotorA2, 0); // left forward
-//   analogWrite(MotorB1, 250); // right forward full speed
-//   analogWrite(MotorB2, 0); // right backward
-//   //delay(550); //495
-//   spin_LeftCheck();
-// }
- 
-// void turn_Left() // turn left with a 90 degree angle
-// {
-//   analogWrite(MotorA2, 255);
-//   analogWrite(MotorB1, 255);
-//   delay(100);
-//   analogWrite(MotorA1, 0); // left backward
-//   analogWrite(MotorA2, 140); // left forward
-//   analogWrite(MotorB1, 255); // right forward
-//   analogWrite(MotorB2, 0);   // right backward
-//   //delay(1800);
-//   turn_LeftCheck();
-// }
- 
-// void turn_Right() // turn right with a 90 degree angle
-// {
-//   analogWrite(MotorA2, 255);
-//   analogWrite(MotorB1, 255);
-//   delay(100);
-//   analogWrite(MotorA1, 0); // left backward
-//   analogWrite(MotorA2, 255); // left forward
-//   analogWrite(MotorB1, 140); // right forward
-//   analogWrite(MotorB2, 0);   // right backward
-//   //delay(2625);
-//   turn_RightCheck();
-// }
-
 #include <Arduino.h>
 
+// Sonar pin defines
 #define trig 4
 #define echo 7
+
+// Motor pin defines
 #define MotorA1 8 // MotorA1 at port 8 digital
-#define MotorA2 10 // MotorA2 at port 10 analog
+#define MotorA2 10 // MotorA2 at port 10 analog 
 #define MotorB1 11 // MotorB1 at port 11 analog
-#define MotorB2 9 // MotorB2 at port 9 analog
- 
+#define MotorB2 12 // MotorB2 at port 12 digital  
+
+#define Gripper 9 // gripper at 9 analog
+#define RotationR1 2 // RotationR1 at port 2 digital
+#define RotationR2 3 // RotationR2 at port 3 digital
+#define DVALUE 10 // debounce value in ms
+#define PIVALUE 3.141592653589793238462643383279502884197 // 39 digits or so
+
+// A1 left backward
+// A2 left forward
+// B1 right forward
+// B2 right backward
+
+// Sensor pin definitions
+int linePins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+
+#define AVERAGE_WINDOW 1  // Number of readings to average
+int recentReadings[8][AVERAGE_WINDOW];
+int readingIndex = 0;
+
+int pwm;
+int amount = 0;
+
+int RightRotation = 0;
+int LeftRotation = 0;
+int angle = 50;
+
+long duration = pulseIn(echo, HIGH);
+int distance = (duration * 0.017); // Distance in cm
+bool flagUp = false;
+
+// Arrays to store sensor values
+int minlineValue[8];
+int maxlineValue[8];
+int avglineValue[8];
+int High_Threshold[8];
+int Low_Threshold[8];
+
+// Sensor states
+String sensorStates[8]; // Holds the current state for each sensor
+
 void forward();
 void backward();
 void stop();
@@ -277,38 +57,175 @@ void spin_Left();
 void turn_Right();
 void turn_Left();
 void sonar();
+void Initializing();
+void calibrateSensors();
+void lineCalibration();
+void determineStates();
+void updaterotation_R1();
+void updaterotation_R2();
+void reset_Rotations();
 
-
-int pwm;
-int amount = 0;
- 
-void setup()
-{
+void setup() {
+ Serial.begin(9600);
+ Initializing();
  pinMode(MotorA1, OUTPUT); // MotorA1 is output
  pinMode(MotorA2, OUTPUT); // MotorA2 is output
  pinMode(MotorB1, OUTPUT); // MotorB1 is output
  pinMode(MotorB2, OUTPUT); // MotorB2 is output
- Serial.begin(9600);
  pinMode(trig, OUTPUT);
  pinMode(echo, INPUT);
- 
+ pinMode(Gripper, OUTPUT);
+ pinMode(RotationR1, INPUT_PULLUP);
+ pinMode(RotationR2, INPUT_PULLUP);
+ attachInterrupt(digitalPinToInterrupt(RotationR1), updaterotation_R1, CHANGE);
+ attachInterrupt(digitalPinToInterrupt(RotationR2), updaterotation_R2, CHANGE);
 }
  
 void loop()
 {
-  // delay(1000);
-  // forward();
-  // stop();
-  // backward();
+  updaterotation_R2();
+  updaterotation_R1();
+  forward();
  
-// 1st
-  // turn_Left();
-  // spin_Right();
-  // turn_Right();
-  // spin_Left();
-  // stop();
- 
+  // if (flagUp = true)
+  // {
+  //   turn_Left();
+  //   spin_Right();
+  //   turn_Right();
+  //   spin_Left();
+  //   stop();
+  // }
   sonar();
+}
+
+void flagCheck()
+{
+  if(distance > 20)
+  {
+    flagUp = true;
+  }
+}
+
+void Initializing(){
+  for (int i = 0; i < 8; i++) {
+    pinMode(linePins[i], INPUT);
+    minlineValue[i] = 1023;  // Initialize with max possible value
+    maxlineValue[i] = 0;     // Initialize with min possible value
+    sensorStates[i] = " "; // Initial state for all sensors
+
+    // Initialize recent readings
+    for (int j = 0; j < AVERAGE_WINDOW; j++) {
+      recentReadings[i][j] = 0;
+    }
+  }
+}
+
+void calibrateSensors() {
+  for (int i = 0; i < 100; i++) {  // Adjust the number of calibration iterations as needed
+    for (int j = 0; j < 8; j++) {
+      int sensorValue = analogRead(linePins[j]);
+
+      // Update minimum and maximum values
+      if (sensorValue < minlineValue[j]) {
+        minlineValue[j] = sensorValue;
+      }
+      if (sensorValue > maxlineValue[j]) {
+        maxlineValue[j] = sensorValue;
+      }
+    }
+    delay(10);
+  }
+
+  // Calculate static thresholds for each sensor
+  for (int i = 0; i < 8; i++) {
+    High_Threshold[i] = (maxlineValue[i] + minlineValue[i]) / 2 + 50; // Black threshold
+    Low_Threshold[i] = (maxlineValue[i] + minlineValue[i]) / 2 + 100; // Adjusted white threshold (higher range)
+
+    Serial.print("Sensor ");
+    Serial.print(i + 1);
+    Serial.print(" -> Min: ");
+    Serial.print(minlineValue[i]);
+    Serial.print(", Max: ");
+    Serial.print(maxlineValue[i]);
+    Serial.print(", High Threshold: ");
+    Serial.print(High_Threshold[i]);
+    Serial.print(", Low Threshold: ");
+    Serial.println(Low_Threshold[i]);
+  }
+}
+
+
+void lineCalibration() {
+  for (int i = 0; i < 8; i++) {
+    int sensorValue = analogRead(linePins[i]);
+
+    // Update recent readings
+    recentReadings[i][readingIndex] = sensorValue;
+
+    // Calculate moving average
+    long sum = 0;
+    for (int j = 0; j < AVERAGE_WINDOW; j++) {
+      sum += recentReadings[i][j];
+    }
+    avglineValue[i] = sum / AVERAGE_WINDOW;
+  }
+
+  // Update reading index
+  readingIndex = (readingIndex + 1) % AVERAGE_WINDOW;
+
+  delay(200); // Delay for stability
+}
+
+void determineStates() {
+  for (int i = 0; i < 8; i++) {
+    if (avglineValue[i] > High_Threshold[i]) {
+      sensorStates[i] = "black";
+    } else if (avglineValue[i] < Low_Threshold[i]) {
+      sensorStates[i] = "white";
+    } else {
+      sensorStates[i] = "unknown"; // For values in between thresholds
+    }
+  }
+}
+
+void updaterotation_R1() // right rotation
+{
+  static unsigned long timer;
+  static bool lastState;
+  noInterrupts();
+  if (millis() > timer) {
+    bool state = digitalRead(RotationR1);
+    if (lastState != state) {
+      RightRotation++;
+      lastState = state;
+    }
+  timer = millis() + DVALUE;
+  }
+  interrupts();
+}
+
+void updaterotation_R2() // left rotation
+{
+  static unsigned long timer;
+  static bool lastState;
+  noInterrupts();
+  if (millis() > timer) {
+    bool state = digitalRead(RotationR2);
+    if (lastState != state) {
+      LeftRotation++;
+      lastState = state;
+    }
+  timer = millis() + DVALUE;
+  }
+  interrupts();
+}
+
+void reset_Rotations() 
+{
+  noInterrupts();
+  RightRotation = 0;
+  LeftRotation = 0;
+  interrupts();
 }
  
 void sonar() // printing distance
@@ -318,9 +235,6 @@ void sonar() // printing distance
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
- 
-  long duration = pulseIn(echo, HIGH);
-  int distance = (duration * 0.034) / 2; // Distance in cm
  
   Serial.print("Distance: ");
   Serial.print(distance);
@@ -353,27 +267,30 @@ void sonar() // printing distance
  
 void forward()
 {
-  digitalWrite(MotorA1, 0);
-  analogWrite(MotorB2, 0);
+  reset_Rotations();
   analogWrite(MotorA2, 250); // Left motor runs at full speed
+  digitalWrite(MotorA1, LOW);
+  digitalWrite(MotorB2, LOW);
  
-  for(int pwm = 255; pwm < 256; pwm++)
+  for(int pwm = 0; pwm < 256; pwm++)
   {
-    analogWrite(MotorB1, pwm - 12); // Right motor with PWM control, slightly reduced
+    analogWrite(MotorB1, pwm + 12); // Right motor with PWM control, slightly reduced
     delay(200);
   }
-  // delay(5000);
+  delay(5000);
 }
  
 void backward()
 {
+  reset_Rotations();
+  // analogWrite(MotorB1,)
   analogWrite(MotorA2, 0);
-  analogWrite(MotorB1, 0);
-  analogWrite(MotorA1, 250); // Left motor runs at full speed
+  digitalWrite(MotorB2, HIGH);
+  digitalWrite(MotorA1, HIGH); // Left motor runs at full speed
  
-  for(int pwm = 255; pwm < 256; pwm++)
+  for(int pwm = 255; pwm > 0; pwm++)
   {
-    analogWrite(MotorB2, pwm -17); // Right motor with PWM control, slightly reduced
+    analogWrite(MotorB1, pwm - 17); // Right motor with PWM control, slightly reduced
     delay(200);
   }
   delay(5000);
@@ -381,53 +298,154 @@ void backward()
  
 void stop() // stop all motors
 {
-  analogWrite(MotorA1, 0);
-  analogWrite(MotorA2, 0);
-  analogWrite(MotorB1, 0);
-  analogWrite(MotorB2, 0);
-  delay(1000);
+  analogWrite(MotorB1, 0);  // right forward
+  digitalWrite(MotorA1, LOW);// left bacward
+  analogWrite(MotorA2, 0);   // left forward
+  digitalWrite(MotorB2, LOW); // right backward
 }
- 
+
 void spin_Right() // spin right on its axle in a 90 degree angle
 {
-  analogWrite(MotorA1, 0); // left backward
-  analogWrite(MotorA2, 255); // left forward full speed
-  analogWrite(MotorB1, 0); // right forward
-  analogWrite(MotorB2, 255); // right backward full speed
+  reset_Rotations();
+  analogWrite(MotorB1, 62);  // right forward
+  digitalWrite(MotorA1, LOW);// left bacward
+  analogWrite(MotorA2, 255);   // left forward
+  digitalWrite(MotorB2, HIGH); // right backward
   delay(510); // 455
 }
  
 void spin_Left() // spin left on its axle in a 90 degree angle
 {
-  analogWrite(MotorA1, 250); // left backward full speed
-  analogWrite(MotorA2, 0); // left forward
-  analogWrite(MotorB1, 250); // right forward full speed
-  analogWrite(MotorB2, 0); // right backward
+  reset_Rotations();  
+   analogWrite(MotorB1, 255);  // right forward
+  digitalWrite(MotorA1, HIGH);// left bacward
+  analogWrite(MotorA2, 22);   // left forward
+  digitalWrite(MotorB2, LOW); // right backward
   delay(550); //495
 }
  
 void turn_Left() // turn left with a 90 degree angle
 {
+  reset_Rotations();
   analogWrite(MotorA2, 255);
   analogWrite(MotorB1, 255);
   delay(100);
-  analogWrite(MotorA1, 0); // left backward
+  digitalWrite(MotorA1, LOW);// left bacward
   analogWrite(MotorA2, 140); // left forward
   analogWrite(MotorB1, 255); // right forward
-  analogWrite(MotorB2, 0);   // right backward
+  digitalWrite(MotorB2, LOW); // right backward
   delay(1800);
 }
  
 void turn_Right() // turn right with a 90 degree angle
 {
+  reset_Rotations();
   analogWrite(MotorA2, 255);
   analogWrite(MotorB1, 255);
   delay(100);
-  analogWrite(MotorA1, 0); // left backward
+  digitalWrite(MotorA1, LOW);// left bacward
   analogWrite(MotorA2, 255); // left forward
   analogWrite(MotorB1, 140); // right forward
-  analogWrite(MotorB2, 0);   // right backward
+  digitalWrite(MotorB2, LOW); // right backward
   delay(2625);
 }
 
 
+
+
+
+
+
+
+
+
+
+// void spin_Left() 
+// { // should be done
+//   reset_Rotations();  
+//   analogWrite(MotorB1, 255);  // right forward
+//   digitalWrite(MotorA1, HIGH);// left bacward
+//   analogWrite(MotorA2, 22);   // left forward
+//   digitalWrite(MotorB2, LOW); // right backward
+// }
+
+// void spin_Right() // spin right on its axle in a 90 degree angle
+// {
+//   reset_Rotations();
+//   analogWrite(MotorB1, 62);  // right forward
+//   digitalWrite(MotorA1, LOW);// left bacward
+//   analogWrite(MotorA2, 255);   // left forward
+//   digitalWrite(MotorB2, HIGH); // right backward
+// }
+
+// void forward()
+// {
+//   reset_Rotations();
+//   analogWrite(MotorB1, 243);  // right forward
+//   digitalWrite(MotorA1, LOW);// left bacward
+//   analogWrite(MotorA2, 255);   // left forward
+//   digitalWrite(MotorB2, LOW); // right backward
+//   //old right 243
+//   // old left 255
+// }
+
+// void backward()
+// {
+//   reset_Rotations();
+//   analogWrite(MotorB1, 0);  // right forward
+//   digitalWrite(MotorA1, HIGH);// left bacward
+//   analogWrite(MotorA2, 0);   // left forward
+//   digitalWrite(MotorB2, HIGH); // right backward
+//   // old right 238
+//   // old left 250
+// }
+
+// void turn_Left() // turn left with a 90 degree angle
+// {
+//   reset_Rotations();
+//   analogWrite(MotorB1, 255);  // right forward
+//   digitalWrite(MotorA1, LOW);// left bacward
+//   analogWrite(MotorA2, 140);   // left forward
+//   digitalWrite(MotorB2, LOW); // right backward
+//   // old left 140
+//   //old right 255
+// }
+
+// void turn_Right()  // turn right with a 90 degree angle
+// { 
+//   reset_Rotations();
+//   analogWrite(MotorB1, 140);  // right forward
+//   digitalWrite(MotorA1, LOW);// left bacward
+//   analogWrite(MotorA2, 255);   // left forward
+//   digitalWrite(MotorB2, LOW); // right backward
+//   // old right 140
+//   // old left 255
+// }
+
+
+// void gripper(int angle) 
+// {
+//   // Map the angle to the pulse width (1000 to 2000 microseconds)
+//   int pulseWidth = map(angle, 0, 180, 1000, 2000);
+//   // Send the PWM signal
+//   for (int i = 0; i < 50; i++) { // Repeat to maintain the pulse for ~1 second
+//     analogWrite(Gripper, 255); // Set the pin high
+//     delayMicroseconds(pulseWidth); // Wait for the pulse width duration
+//     analogWrite(Gripper, 0); // Set the pin low
+//     delayMicroseconds(20000 - pulseWidth); // Wait for the rest of the 20ms period
+//   }
+// }
+
+// void printSensorStates() {
+//   for (int i = 0; i < 8; i++) {
+//     Serial.print("Sensor ");
+//     Serial.print(i + 1);
+//     Serial.print(": ");
+//     Serial.print("Avg = ");
+//     Serial.print(avglineValue[i]);
+//     Serial.print(" -> State: ");
+//     Serial.println(sensorStates[i]);
+//   }
+//   Serial.println(" ");
+//   delay(500); // Delay for readability in the Serial Monitor
+// }
